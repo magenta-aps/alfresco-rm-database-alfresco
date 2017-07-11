5810c4a3832be03ea1444d2d07b23e543919b34e
AlexanderMomtaz <alexander@magenta.dk>
2017-07-11 15:47:01 +0200

    Added key for forensic psyc dec

--- src/main/java/dk/magenta/model/DatabaseModel.java
+++ src/main/java/dk/magenta/model/DatabaseModel.java
@@ -14,10 +14,11 @@ public interface DatabaseModel {
     public static String RM_MODEL_URI = "http://www.rm.dk/model/database/1.0";
     public static String RM_MODEL_PREFIX = "rm";
 
-    public static String TYPE_FORENSIC_PSYC_DEC = "forensicPsychiatryDeclaration";
-
     public static QName PROP_CASE_NUMBER = QName.createQName(RM_MODEL_URI, "caseNumber");
     public static QName PROP_LINK = QName.createQName(CONTENT_MODEL_URI, "link");
+
+    public static String TYPE_FORENSIC_PSYC_DEC = "forensicPsychiatryDeclaration";
+    public static String TYPE_FORENSIC_PSYC_DEC_KEY = PROP_CASE_NUMBER.getLocalName();
 
     /**
      * caselink properties
