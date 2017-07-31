1015f365fe4bd0f4ba9418b7e5870ace194eaf02
AlexanderMomtaz <alexander@magenta.dk>
2017-07-31 09:45:10 +0200

    Added unlock method for entries.

---
+++ src/main/java/dk/magenta/webscripts/entries/UnlockEntry.java
@@ -0,0 +1,45 @@
+package dk.magenta.webscripts.entries;
+
+import dk.magenta.beans.EntryBean;
+import dk.magenta.utils.JSONUtils;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.json.JSONObject;
+import org.springframework.extensions.webscripts.AbstractWebScript;
+import org.springframework.extensions.webscripts.WebScriptRequest;
+import org.springframework.extensions.webscripts.WebScriptResponse;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+public class UnlockEntry extends AbstractWebScript {
+
+    private EntryBean entryBean;
+    public void setEntryBean(EntryBean entryBean) {
+        this.entryBean = entryBean;
+    }
+
+    @Override
+    public void execute(WebScriptRequest webScriptRequest, WebScriptResponse webScriptResponse) throws IOException {
+        Map<String, String> params = JSONUtils.parseParameters(webScriptRequest.getURL());
+
+        webScriptResponse.setContentEncoding("UTF-8");
+        Writer webScriptWriter = webScriptResponse.getWriter();
+        JSONObject result;
+
+        try {
+            String uuid = params.get("uuid");
+            NodeRef nodeRef = entryBean.getNodeRef(uuid);
+            entryBean.unlockEntry(nodeRef);
+            result = JSONUtils.getSuccess();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = JSONUtils.getError(e);
+            webScriptResponse.setStatus(400);
+        }
+        JSONUtils.write(webScriptWriter, result);
+    }
+}
+
+// F.eks. curl -i -u admin:admin -X PUT 'http://localhost:8080/alfresco/s/entry/unlock?uuid=445644-4545-4564-8848-1849155'
