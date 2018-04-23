d3f85458c1c81d45e4bb0fb7635fd68565ee9478
flemmingheidepedersen <fhp@magenta-aps.dk>
2018-04-23 14:03:30 +0200

    extended the output

--- src/main/java/dk/magenta/webscripts/user/UserPermissions.java
+++ src/main/java/dk/magenta/webscripts/user/UserPermissions.java
@@ -1,5 +1,6 @@
 package dk.magenta.webscripts.user;
 
+import com.google.gdata.data.Person;
 import dk.magenta.beans.PropertyValuesBean;
 import dk.magenta.utils.JSONUtils;
 
@@ -7,6 +8,7 @@ import org.alfresco.query.PagingResults;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.security.AuthorityService;
 import org.alfresco.service.cmr.security.MutableAuthenticationService;
 import org.alfresco.service.cmr.security.PersonService;
 import org.alfresco.service.cmr.site.SiteService;
@@ -23,10 +25,19 @@ import java.io.Writer;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-public class ListOfDeactivatedUsers extends AbstractWebScript {
+
+public class UserPermissions extends AbstractWebScript {
 
     private PropertyValuesBean propertyValuesBean;
+
+    public void setAuthorityService(AuthorityService authorityService) {
+        this.authorityService = authorityService;
+    }
+
+    private AuthorityService authorityService;
+
 
     public void setPersonService(PersonService personService) {
         this.personService = personService;
@@ -47,6 +58,26 @@ public class ListOfDeactivatedUsers extends AbstractWebScript {
     }
 
     SiteService siteService;
+
+
+    private JSONObject isMember(String user) throws JSONException {
+
+        JSONObject o = new JSONObject();
+        o.put("GROUP_site_retspsyk_SiteEntryLockManager",false);
+        o.put("GROUP_site_retspsyk_SiteRoleManager",false);
+        o.put("GROUP_site_retspsyk_SitePropertyValueManager",false);
+        o.put("GROUP_site_retspsyk_TemplateFolderValueManager",false);
+
+
+        Set<String> auths = authorityService.getAuthoritiesForUser(user);
+        Iterator<String> authIt = auths.iterator();
+        while (authIt.hasNext()){
+            o.put(authIt.next(), true);
+        }
+
+        return o;
+
+    }
 
 
     @Override
@@ -77,12 +108,27 @@ public class ListOfDeactivatedUsers extends AbstractWebScript {
             boolean member =  siteService.isMember("retspsyk", (String)nodeService.getProperty(n, ContentModel.PROP_USERNAME));
 
             JSONObject o = new JSONObject();
+
+            PersonService.PersonInfo authorityName = personService.getPerson(n);
+
             try {
                 o.put("nodeRef", n);
                 o.put("userName", name);
                 o.put("firstName", (String)nodeService.getProperty(n, ContentModel.PROP_FIRSTNAME));
-                o.put("lastName", (String)nodeService.getProperty(n, ContentModel.PROP_LASTNAME));
+                o.put("lastName", (String) nodeService.getProperty(n, ContentModel.PROP_LASTNAME));
                 o.put("active", member);
+
+
+                JSONObject membership = isMember(authorityName.getUserName());
+                System.out.println("the membership");
+                System.out.println(membership);
+
+
+                o.put("GROUP_site_retspsyk_SiteEntryLockManager", membership.getBoolean("GROUP_site_retspsyk_SiteEntryLockManager"));
+                o.put("GROUP_site_retspsyk_SitePropertyValueManager", membership.getBoolean("GROUP_site_retspsyk_SitePropertyValueManager"));
+                o.put("GROUP_site_retspsyk_SiteRoleManager", membership.getBoolean("GROUP_site_retspsyk_SiteRoleManager"));
+                o.put("GROUP_site_retspsyk_TemplateFolderValueManager", membership.getBoolean("GROUP_site_retspsyk_TemplateFolderValueManager"));
+
                 array.put(o);
             } catch (JSONException e) {
                 e.printStackTrace();
