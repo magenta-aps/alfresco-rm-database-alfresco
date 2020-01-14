4f3c61827b775c4658d45a0d993d1b8b11285f0d
Flemming Heide Pedersen <fhp@magenta-aps.dk>
2020-01-14 14:30:15 +0100

    test on staging

---
+++ src/main/java/dk/magenta/webscripts/systemsettings/Settings.java
@@ -0,0 +1,55 @@
+package dk.magenta.webscripts.systemsettings;
+
+import dk.magenta.beans.SettingsBean;
+import dk.magenta.utils.JSONUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.extensions.webscripts.AbstractWebScript;
+import org.springframework.extensions.webscripts.WebScriptRequest;
+import org.springframework.extensions.webscripts.WebScriptResponse;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class Settings extends AbstractWebScript {
+
+    final Logger logger = LoggerFactory.getLogger(Settings.class);
+
+    public void setSettingsBean(SettingsBean settingstBean) {
+        this.settingsBean = settingstBean;
+    }
+
+    private SettingsBean settingsBean;
+
+    @Override
+    public void execute(WebScriptRequest webScriptRequest, WebScriptResponse webScriptResponse) throws IOException   {
+
+        JSONObject result = new JSONObject();
+
+        try {
+
+            String defaultText = settingsBean.getDefaultMailText();
+
+            result.put("text", defaultText);
+
+
+
+
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = JSONUtils.getError(e);
+            webScriptResponse.setStatus(400);
+        }
+
+        JSONUtils.write(webScriptResponse.getWriter(), result);
+
+
+
+    }
+}
