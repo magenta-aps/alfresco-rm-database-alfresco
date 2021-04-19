980a20e75a4c7cd758ff7d1bb1dd2841c545293c
Flemming Heide Pedersen <fhp@magenta-aps.dk>
2021-04-19 11:25:07 +0200

    wip

---
+++ src/main/java/dk/magenta/scheduledjob/WaitReportScheduledJobExecuter.java
@@ -0,0 +1,40 @@
+package dk.magenta.scheduledjob;
+
+import dk.magenta.beans.ReportWaitingTimeBean;
+import dk.magenta.beans.StatBean;
+import org.alfresco.service.ServiceRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WaitReportScheduledJobExecuter {
+    private static final Logger LOG = LoggerFactory.getLogger(WaitReportScheduledJobExecuter.class);
+
+    /**
+     * Public API access
+     */
+    private ServiceRegistry serviceRegistry;
+
+
+    public void setReportWaitingTimeBean(ReportWaitingTimeBean reportWaitingTimeBean) {
+        this.reportWaitingTimeBean = reportWaitingTimeBean;
+    }
+
+    private ReportWaitingTimeBean reportWaitingTimeBean;
+
+    public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+        this.serviceRegistry = serviceRegistry;
+    }
+
+    /**
+     * Executer implementation
+     */
+    public void execute() {
+        LOG.info("Running the scheduled job");
+
+
+
+
+//        statBean.writeToDocument();
+
+    }
+}
