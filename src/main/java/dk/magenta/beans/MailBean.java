e5a60146d1e017d55c03bade61f821a70256ca2b
fhp <fhp@magenta-aps.dk>
2022-02-10 14:13:58 +0100

    fixes bug in filename

--- src/main/java/dk/magenta/beans/MailBean.java
+++ src/main/java/dk/magenta/beans/MailBean.java
@@ -207,7 +207,7 @@ public class MailBean {
                         fileName = nodeService.getProperty(attachmentNodeRef, ContentModel.PROP_NAME).toString() + ".pdf";
 
                         // fixes name problem that causes errors in sending nodeRefs with signatures #48567
-                        FileInfo newFile = fileFolderService.copy(transformed, folder, "underskrevet" + fileName);
+                        FileInfo newFile = fileFolderService.copy(transformed, folder, "underskrevet_" + fileName);
                     }
                     else {
                         // only transform odf, #47443
